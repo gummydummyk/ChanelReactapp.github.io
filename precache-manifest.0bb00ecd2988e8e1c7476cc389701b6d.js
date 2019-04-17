7a2eb91b378ce1789bf05dcd29ad1dc87a9278e3
chanel <chanel@nn.com>
2019-04-17 12:23:36 +0800

    Updates

---
+++ precache-manifest.0bb00ecd2988e8e1c7476cc389701b6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myreactapp2/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c8ada338609b5d2db403",
+    "url": "/myreactapp2/static/js/runtime~main.dae15c41.js"
+  },
+  {
+    "revision": "51cee656593b7b7b6f4d",
+    "url": "/myreactapp2/static/js/main.96a1c7be.chunk.js"
+  },
+  {
+    "revision": "443637cb4c037aa15ccc",
+    "url": "/myreactapp2/static/js/2.1ed43d11.chunk.js"
+  },
+  {
+    "revision": "51cee656593b7b7b6f4d",
+    "url": "/myreactapp2/static/css/main.840ad067.chunk.css"
+  },
+  {
+    "revision": "9704f420c41e5c474b22fe1a3f91acae",
+    "url": "/myreactapp2/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ChanelReactapp.github.io/precache-manifest.62d99250a9d944d764ba197c95a7cea7.js"
+  "/myreactapp2/precache-manifest.0bb00ecd2988e8e1c7476cc389701b6d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/ChanelReactapp.github.io/index.html", {
+workbox.routing.registerNavigationRoute("/myreactapp2/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
