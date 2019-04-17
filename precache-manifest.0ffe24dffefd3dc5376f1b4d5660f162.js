780ce62a9e54f54a037b13381bacf5e01d5af839
chanel <chanel@nn.com>
2019-04-17 12:02:24 +0800

    Updates

---
+++ precache-manifest.0ffe24dffefd3dc5376f1b4d5660f162.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gummydummyk/ChanelReactapp.github.io/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0b566d9b200ce83e6658",
+    "url": "/gummydummyk/ChanelReactapp.github.io/static/js/runtime~main.5d0066a4.js"
+  },
+  {
+    "revision": "7ede79de371fe281c3e5",
+    "url": "/gummydummyk/ChanelReactapp.github.io/static/js/main.2c8b1f74.chunk.js"
+  },
+  {
+    "revision": "443637cb4c037aa15ccc",
+    "url": "/gummydummyk/ChanelReactapp.github.io/static/js/2.1ed43d11.chunk.js"
+  },
+  {
+    "revision": "7ede79de371fe281c3e5",
+    "url": "/gummydummyk/ChanelReactapp.github.io/static/css/main.840ad067.chunk.css"
+  },
+  {
+    "revision": "ffe2d5c2d7d15f2c32d107a2e27556a6",
+    "url": "/gummydummyk/ChanelReactapp.github.io/index.html"
+  }
+];
